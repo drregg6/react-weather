4246997badd2957bcb8bff9b819677a8c2c84091
Dave Regg <drregg6@gmail.com>
2020-04-23 16:28:24 -0400

    New API request

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+/*
+
+TODO:
+= Setup a couple of APIs (if available)
+    - Search for user's location when they enter the site
+    - Allow user to search for city, then use that city name to get lat/long
+= Design
+    - How will this all come together?
+    - Click on a day to get full information?
+= New tech
+    - Tailwind?
+
+*/
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
@@ -10,9 +24,10 @@ import Weather from './components/weather/Weather';
 function App() {
   const [ weather, setWeather ] = useState({})
   useEffect(() => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=philadelphia&appid=${process.env.REACT_APP_WEATHER_API}`;
-    console.log(url)
-    axios.get(url)
+    const altUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=39.95&lon=-75.16&appid=${process.env.REACT_APP_WEATHER_API}&units=imperial`;
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=philadelphia&appid=${process.env.REACT_APP_WEATHER_API}`;
+    console.log(altUrl)
+    axios.get(altUrl)
     .then(res => {
       setWeather({...res.data});
     })
@@ -20,7 +35,7 @@ function App() {
       console.error(err);
     });
   }, []);
-  
+
   return (
     <div className="container">
       <Header />
--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Weather = ({
-  weather: { name }
-}) => {
+const Weather = ({ weather }) => {
+
+  const { current, daily, hourly } = weather;
+  console.log(weather)
+  console.log(current)
+  console.log(daily)
+  console.log(hourly)
+
   return (
     <div className="weather">
-      <h1>{name}</h1>
+      <h1>{weather.timezone}</h1>
     </div>
   )
 }
